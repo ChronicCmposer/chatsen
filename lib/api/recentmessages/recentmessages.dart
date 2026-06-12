2f2ad79930d024205610ba1859ff169d2b46c718
ChronicCmposer <265143892+ChronicCmposer@users.noreply.github.com>
2026-06-12 13:20:09 -0400

    temp fix for recent-messages integration

--- lib/api/recentmessages/recentmessages.dart
+++ lib/api/recentmessages/recentmessages.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class RecentMessages {
   static Future<List<String>> channel(String channelName) async {
-    final response = await http.get(Uri.parse('https://recent-messages.robotty.de/api/v2/recent-messages/$channelName'));
+    final response = await http.get(Uri.parse('https://recent-messages.zneix.eu/api/v2/recent-messages/$channelName'));
     final responseJson = json.decode(utf8.decode(response.bodyBytes));
     return List<String>.from(responseJson['messages']);
   }
